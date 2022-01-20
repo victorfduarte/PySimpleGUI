0922b8139574f5b5c7586926078ea30bcea2362e
Paulo Victor <victorfduarte18@gmail.com>
2022-01-20 12:17:48 -0300

    checkbox e radiobutton

--- tela.py
+++ tela.py
@@ -4,20 +4,39 @@ class TelaPython:
     def __init__(self):
         # Layout
         layout = [
-            [sg.Text('Nome'), sg.Input()],
-            [sg.Text('Idade'), sg.Input()],
+            [sg.Text('Nome', size=(5,0)), sg.Input(size=(15,0), key='nome')],
+            [sg.Text('Idade', size=(5,0)), sg.Input(size=(15,0), key='idade')],
+            [sg.Text('Quais provedores de E-mail são aceitos?')],
+            [sg.Checkbox('Gmail', key='gmail'), sg.Checkbox('Outlook', key='outlook'), sg.Checkbox('Yahoo', key='yahoo')],
+            [sg.Text('Aceita cartão')],
+            [sg.Radio('Sim', 'cartoes', key='aceitaCartao'), sg.Radio('Não', 'cartoes', key='naoAceitaCartao')],
             [sg.Button('Enviar')]
         ]
 
         # Janela
-        janela = sg.Window('Dados do Usuário').layout(layout)
+        janela = sg.Window('Dados do Usuário', layout)
 
         # Extrair os dados da tela
         self.button, self.values = janela.Read()
 
 
     def Iniciar(self):
-        print(self.values)
+        nome = self.values['nome']
+        idade = self.values['idade']
+        aceita_gmail = self.values['gmail']
+        aceita_outlook = self.values['outlook']
+        aceita_yahoo = self.values['yahoo']
+        aceita_cartao = self.values['aceitaCartao']
+        nao_aceita_cartao = self.values['naoAceitaCartao']
+
+        print(f'nome: {nome}')
+        print(f'idade: {idade}')
+        print(f'aceita Gmail: {aceita_gmail}')
+        print(f'aceita Outlook: {aceita_outlook}')
+        print(f'aceita Yahoo: {aceita_yahoo}')
+        print(f'aceita cartão: {aceita_cartao}')
+        print(f'não aceita cartão: {nao_aceita_cartao}')
+
 
 
 tela = TelaPython()
