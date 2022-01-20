3b86f33d931f98add4bce72a02dbec2fd525322e
Paulo Victor <victorfduarte18@gmail.com>
2022-01-20 16:10:18 -0300

    versão final

--- tela.py
+++ tela.py
@@ -2,6 +2,7 @@ import PySimpleGUI as sg
 
 class TelaPython:
     def __init__(self):
+        sg.change_look_and_feel('DarkBlue')
         # Layout
         layout = [
             [sg.Text('Nome', size=(5,0)), sg.Input(size=(15,0), key='nome')],
@@ -10,32 +11,37 @@ class TelaPython:
             [sg.Checkbox('Gmail', key='gmail'), sg.Checkbox('Outlook', key='outlook'), sg.Checkbox('Yahoo', key='yahoo')],
             [sg.Text('Aceita cartão')],
             [sg.Radio('Sim', 'cartoes', key='aceitaCartao'), sg.Radio('Não', 'cartoes', key='naoAceitaCartao')],
-            [sg.Button('Enviar')]
+            [sg.Slider((0,255), 0, orientation='h', size=(15,20), key='sliderVelocidade')],
+            [sg.Button('Enviar')],
+            [sg.Output((30,20))]
         ]
 
         # Janela
-        janela = sg.Window('Dados do Usuário', layout)
-
-        # Extrair os dados da tela
-        self.button, self.values = janela.Read()
+        self.janela = sg.Window('Dados do Usuário', layout)
 
 
     def Iniciar(self):
-        nome = self.values['nome']
-        idade = self.values['idade']
-        aceita_gmail = self.values['gmail']
-        aceita_outlook = self.values['outlook']
-        aceita_yahoo = self.values['yahoo']
-        aceita_cartao = self.values['aceitaCartao']
-        nao_aceita_cartao = self.values['naoAceitaCartao']
+        while True:
+            # Extrair os dados da tela
+            self.button, self.values = self.janela.Read()
 
-        print(f'nome: {nome}')
-        print(f'idade: {idade}')
-        print(f'aceita Gmail: {aceita_gmail}')
-        print(f'aceita Outlook: {aceita_outlook}')
-        print(f'aceita Yahoo: {aceita_yahoo}')
-        print(f'aceita cartão: {aceita_cartao}')
-        print(f'não aceita cartão: {nao_aceita_cartao}')
+            nome = self.values['nome']
+            idade = self.values['idade']
+            aceita_gmail = self.values['gmail']
+            aceita_outlook = self.values['outlook']
+            aceita_yahoo = self.values['yahoo']
+            aceita_cartao = self.values['aceitaCartao']
+            nao_aceita_cartao = self.values['naoAceitaCartao']
+            slider_velocidade = self.values['sliderVelocidade']
+
+            print(f'nome: {nome}')
+            print(f'idade: {idade}')
+            print(f'aceita Gmail: {aceita_gmail}')
+            print(f'aceita Outlook: {aceita_outlook}')
+            print(f'aceita Yahoo: {aceita_yahoo}')
+            print(f'aceita cartão: {aceita_cartao}')
+            print(f'não aceita cartão: {nao_aceita_cartao}')
+            print(f'slider velocidade: {slider_velocidade}')
 
 
 
